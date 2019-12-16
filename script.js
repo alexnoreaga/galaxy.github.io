5d05a6c3bbca33ee51cab90c3aebba6c1b41d154
alexnoreaga <35931297+alexnoreaga@users.noreply.github.com>
2019-12-16 17:37:14 +0700

    Update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var bulan = "Oktober 2019"
-var statusgaji = "Status : TRANSFER BERHASIL 31/10/2019"
-var statusbonus = "Status : TRANSFER BERHASIL 15/11/2019"
+var bulan = "Desember 2019"
+var statusgaji = "Status : TRANSFER BERHASIL 01/12/2019"
+var statusbonus = "Status : TRANSFER BERHASIL 16/12/2019"
 
 document.getElementById("bulan").innerHTML = bulan;
 document.getElementById("statusgaji").innerHTML = statusgaji;
